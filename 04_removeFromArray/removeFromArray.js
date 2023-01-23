783dd2b08715cf76a7b8d78f7802f68192ed0391
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-23 18:33:51 -0500

    yeah I failed this one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    let newArray= [];
 
-};
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!values.includes(item)) {
+          newArray.push(item);
+        }
+      });
+      // and return that array
+      return newArray;
+
+}   
 
 // Do not edit below this line
 module.exports = removeFromArray;
