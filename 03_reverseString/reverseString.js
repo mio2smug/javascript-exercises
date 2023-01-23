e64ddbaa2f888a77a1d9def8e1babd9ef5db5d74
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-23 17:55:26 -0500

    pass test 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {  //Assuming string is hello
+    let reverseString = "";  
+    for(let i = string.length; i + 1 > 0 ; i--){ //set index to length of hello (6), 
+        reverseString += string.charAt(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
