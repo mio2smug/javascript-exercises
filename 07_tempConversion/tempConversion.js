5547ce536f0292da2769da66a0541eab8cab7dee
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-23 20:58:53 -0500

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    var temperatureInCelsius = ((temp-32)*(5/9));
+    temperatureInCelsius =  Math.round(temperatureInCelsius * 10) / 10;
+  return temperatureInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var temperatureInFahrenheit = ((temp*(9/5))+32);
+  temperatureInFahrenheit = Math.round(temperatureInFahrenheit *10) / 10;
+  return temperatureInFahrenheit;
 };
 
 // Do not edit below this line
