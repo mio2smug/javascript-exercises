4b793ec19dbf1704e488a1a7ff23ab72deebc90b
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-30 18:46:32 -0500

    alindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let string = input.toLowerCase().
+    replace(/[^a-z]/g, "");
 
+    let reverseString = string.split("").reverse().join("");
+    return (string === reverseString); 
 };
 
 // Do not edit below this line
