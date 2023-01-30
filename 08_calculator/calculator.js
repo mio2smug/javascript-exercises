e88c0d331c20d59584a245ffb802346e6d415c1c
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-30 17:54:03 -0500

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(integerA,integerB) {
+	return integerA+integerB;
 };
 
-const subtract = function() {
-	
+const subtract = function(integerA, integerB) {
+	return integerA-integerB;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array[0] == "" ){return 0 ;}
+
+  let sumTotal = array.reduce((partialSum, a) => partialSum + a, 0);
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0 ;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+
+
+const factorial = function(integer) {
+  let factorialTotal = 1;
+  if (integer == 0){
+    return 1;
+  }
+  else{
+  for (let i = integer; i>0 ;i--){
+    factorialTotal *= i;
+  }
+  return factorialTotal;
+}
 };
 
 // Do not edit below this line
