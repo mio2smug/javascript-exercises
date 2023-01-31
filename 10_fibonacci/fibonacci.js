ba844ca178337fbb6426201735c9de172e114027
mio2smug <bryan.lazarowork@yahoo.com>
2023-01-30 19:19:17 -0500

    passed test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(integer) {
+//use ==, test will throw string must use type conversion
+if (integer < 0 ) return 'OOPS';
+let fibonacci = [0,1];
+
+for(let i = 2; i < integer+1; i++){
+    fibonacci[i] = fibonacci[i-2] + fibonacci[i - 1];
+}
+return fibonacci[integer];
 
 };
 
